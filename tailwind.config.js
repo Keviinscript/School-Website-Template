48a3de72a014f530fb3201d2f2eb5d64ac8695d7
ikechukwu kevin-chika <101973303+Keviinscript@users.noreply.github.com>
2022-09-24 19:00:00 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/*.{html,js}'],
+  theme: {
+    fontFamily: {
+      alegreya: [ 'Alegreya Sans', 'sans-serif'],
+      poppins: ['Poppins', 'sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
